577b370082aca4d33e54f497b5164f5f8e5193d5
Stefan Holodnick <stefan.holodnick@woodlanddirect.com>
2017-11-07 16:43:07 -0500

    Added phone numbers handler.

--- wd-email-form.js
+++ wd-email-form.js
@@ -3,11 +3,52 @@ var ses = new AWS.SES()
 
 // Exports sendEmail functions which sends form information
 exports.sendEmail = (e, ctx) => {
+  var numbers = {
+    "Andrew Dering": "andrew.dering@woodlanddirect.com",
+    "Andrew Kern": "andrew.kern@woodlanddirect.com",
+    "Barry Blake": "barry.blake@woodlanddirect.com",
+    "Ben Morris": "ben@woodlanddirect.com",
+    "Bill Hylton": "bill.hylton@woodlanddirect.com",
+    "Brett Decker": "Brett.Decker@WoodlandDirect.com",
+    "Brett Lundin": "Brett.Lundin@WoodlandDirect.com",
+    "Christian Vanover": "christian.vanover@woodlanddirect.com",
+    "Chuck Nitschke": "chuck.nitschke@woodlanddirect.com",
+    "Daniel Lombardo": "daniel.lombardo@woodlanddirect.com",
+    "Danny Landon": "danny.landon@woodlanddirect.com",
+    "Derrick Barnes": "derrick.barnes@woodlanddirect.com",
+    "Devin Inbody": "devin.inbody@woodlanddirect.com",
+    "Donavon Rigel": "donavon.rigel@woodlanddirect.com",
+    "Garrett Jamieson": "garrett.jamieson@woodlanddirect.com",
+    "James Doto": "James.Doto@WoodlandDirect.com",
+    "Jason Hurley": "jason.hurley@woodlanddirect.com",
+    "Jeff O'Keefe": "jeff.okeefe@woodlanddirect.com",
+    "Jeremiah Bouchard": "jeremiah.bouchard@woodlanddirect.com",
+    "Josh Leake": "josh.leake@woodlanddirect.com",
+    "Ken Colley": "ken.colley@woodlanddirect.com",
+    "Kevin Pottenger": "kevin.pottenger@woodlanddirect.com",
+    "Lynden Nugteren": "lynden.nugteren@woodlanddirect.com",
+    "Mark Matis": "Mark.Matis@woodlanddirect.com",
+    "Matthew Murray": "matthew.murray@woodlanddirect.com",
+    "Mike Jergens": "mike.jergens@woodlanddirect.com",
+    "Nathan Roethe": "nathan.roethe@woodlanddirect.com",
+    "Nicholas Hodge": "nick.hodge@woodlanddirect.com",
+    "Nick Carrier": "nick.carrier@woodlanddirect.com",
+    "Nick Janco": "nick.janco@woodlanddirect.com",
+    "Randy Mowry": "randy.mowry@woodlanddirect.com",
+    "Rogers Trotter": "rogers.trotter@woodlanddirect.com",
+    "Ryan Brady": "ryan@woodlanddirect.com",
+    "Seth Kester": "seth.kester@woodlanddirect.com",
+    "Thomas Kearney": "thomas.kearney@woodlanddirect.com",
+    "default": "marty.dorda@woodlanddirect.com"
+  }
+
+  var receiverEmail = (e.receiver_name) ? numbers[e.receiver_name] : e.receiver_email_address
+
   // Create email parameters
   var params = {
     Destination: {
       ToAddresses: [
-        e.receiver_email_address
+        receiverEmail
       ]
     },
     Message: {
